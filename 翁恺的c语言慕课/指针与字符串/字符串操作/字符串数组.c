3f9fb8992063d8efb8ca8acc4ade437c3ff21079
SWY <1905447215@qq.com>
2024-10-04 11:35:06 +0800

    加入了char *a[]与char a[][number]的区别的注释

--- 翁恺的c语言慕课/指针与字符串/字符串操作/字符串数组.c
+++ 翁恺的c语言慕课/指针与字符串/字符串操作/字符串数组.c
@@ -1,7 +1,8 @@
 /*字符串数组*/
 /*char *a
 a是一个指针，指向另一个指针，那一个指针指向一个字符(串)
-char a[][number]*/
+char a[][number]
+char *a[]与char a[][number]是不一样的，前者的每一个单元都是一个字符串指针*/
 #include<stdio.h>
 int main(int argc,char const *argv[])
 {
