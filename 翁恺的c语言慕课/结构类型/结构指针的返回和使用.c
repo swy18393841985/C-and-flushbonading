cacce1957b807df30e927cac9adc8570417122d6
SWY <1905447215@qq.com>
2024-10-08 22:28:06 +0800

    结构指针

---
+++ 翁恺的c语言慕课/结构类型/结构指针的返回和使用.c
@@ -0,0 +1,50 @@
+#include<stdio.h>
+struct point{
+    int x;
+    int y;
+};
+
+struct point * getStruct(struct point*);
+//struct point getStruct(void);
+//void output(struct point*);
+void output(struct point);
+void print(const struct point *p);
+
+int main()
+{
+    struct point y={0,0};
+    //struct point *pStruct=&y;
+    //getStruct(pStruct);
+    //output (pStruct);
+    getStruct(&y);
+    printf("1\n");
+    output(y);
+    printf("2\n");
+    output(*getStruct(&y));
+    printf("3\n");
+    print(getStruct(&y));
+    return 0;
+}
+
+//void getStruct(struct point *p)
+struct point* getStruct(struct point *p)
+{
+    // struct point p;
+    // scanf("%d",&p.x);
+    // scanf("%d",&p.y);
+    // printf("%d %d\n",p.x,p.y);
+    // return p;
+    scanf("%d",&p->x);
+    scanf("%d",&p->y);
+    printf("%d,%d\n",p->x,p->y);
+    return p;
+}
+// void output(struct point *p){
+//     printf("%d %d",p->x,p->y);
+// }
+void output(struct point p){
+    printf("%d,%d\n",p.x,p.y);
+}
+void print(const struct point *p){
+    printf("%d %d\n",p->x,p->y);
+}
