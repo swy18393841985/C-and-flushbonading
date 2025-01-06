f5185ed7953c6ace032cc1761831e782f6d80982
SWY <1905447215@qq.com>
2025-01-06 22:35:32 +0800

    啊

--- 翁恺的c语言慕课/链表/linked_list/ergodic_linked_list.c
+++ 翁恺的c语言慕课/链表/linked_list/ergodic_linked_list.c
@@ -4,36 +4,41 @@
 
 typedef struct _list{
     Node *head;
-    //Node *tail;
+    Node *tail;
 }List;
+
+void add(List *pList,int number);
 
 int main()
 {
     List list;
-    list.head=NULL;
+    list.head=list.tail=NULL;
     int number;
     do{
         scanf("%d",&number);
         if(number!=-1){
-           add(&list,number);      
+            add(&list,number);      
         }
     }while(number!=-1);
     return 0;
 } 
-void add(List *pList,int number){
+
+void add(List *pList,int number)
+{
     //add to Linked_list
     Node *p=(Node*)malloc(sizeof(Node));
+    if(p==NULL){
+        fprintf(stderr, "Memory allocation failed\n");
+        exit(EXIT_FAILURE);
+    }
     p->value=number;
     p->next=NULL;
-    //find the last
-    Node *last=pList->head;
-    if (last){
-        while(last->next){
-            last=last->next;
-        }
-        //attach
-        last->next=p;
-    }else{
+    if(pList->head==NULL){
         pList->head=p;
+        pList->tail=p;
+    }else{
+        pList->tail->next=p;
+        pList->tail=p;
     }
+       
 }
