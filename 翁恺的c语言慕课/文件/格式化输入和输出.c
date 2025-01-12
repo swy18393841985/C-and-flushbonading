17417b651b84fd57bdc6ea89be0512c6406aab81
SWY <1905447215@qq.com>
2025-01-12 17:36:41 +0800

    头哥

--- 翁恺的c语言慕课/文件/格式化输入和输出.c
+++ 翁恺的c语言慕课/文件/格式化输入和输出.c
@@ -15,5 +15,25 @@ int main()
     int num1;
     scanf("%*d%d",&num1);
     printf("%d\n",num1);
+    const double PI = 3.141592653589;
+    double radius, area, perimeter;
+    scanf("%lf", &radius);
+    area = PI * radius * radius;
+    perimeter = 2 * radius * PI;
+    printf("Radius = %6.2f\n", radius);
+    printf("Area = %6.2f\n", area);
+    printf("Perimeter = %6.2f\n\n", perimeter);
+    printf("Radius    = %6.2f\n", radius);
+    printf("Area      = %6.2f\n", area);
+    printf("Perimeter = %6.2f\n\n", perimeter);
+    printf("   Radius = %6.2f\n", radius);
+    printf("     Area = %6.2f\n", area);
+    printf("Perimeter = %6.2f\n\n", perimeter);
+    printf("%12s = %6.2f\n", "Radius", radius);
+    printf("%12s = %6.2f\n", "area", area);
+    printf("%12s = %6.2f\n\n", "perimeter", perimeter);
+    printf("%-12s = %6.2f\n", "Radius", radius);
+    printf("%-12s = %6.2f\n", "area", area);
+    printf("%-12s = %6.2f\n", "perimeter", perimeter);
     return 0;
 }
