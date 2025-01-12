8fdd043bce4874e49eb336fe551e3b35fedb750f
SWY <1905447215@qq.com>
2025-01-12 12:59:05 +0800

    加了点东西

--- 翁恺的c语言慕课/程序结构/大程序结构/max.h
+++ 翁恺的c语言慕课/程序结构/大程序结构/max.h
@@ -5,3 +5,16 @@ int max(int a,int b);
 extern int gAll;
 
 #endif
+/*前向声明
+#ifndef __LIST_HEAD__
+#define __LIST_HEAD__
+
+struct Node;
+
+typedef struct_list{
+    struct Node *head;
+    struct Node *tail;
+}
+#endif
+这个地方不知道Node是怎样的，所以可以用struct Node来告诉编译器Node是一个结构
+*/
